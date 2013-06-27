71ce5ee535431273bdaa2ba397a5cd35c9435bba
Chris Roos <chris@seagul.co.uk>
2013-06-27 12:52:16 +0100

    Add basic progress bar for file uploads
    If the file is small then the progress bar will fill up immediately. To see it
    in action you'll probably want to test with a large file.

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,3 +1,11 @@
 $(function() {
-  $('form.edit_person').fileupload({});
+  $('form.edit_person').fileupload({
+    progressall: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      $('#progress .bar').css(
+          'width',
+          progress + '%'
+      );
+    }
+  });
 })
