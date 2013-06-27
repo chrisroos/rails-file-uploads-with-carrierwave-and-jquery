50016cb4a3647a587593e5956d436b98328db4c8
Chris Roos <chris@seagul.co.uk>
2013-06-27 12:11:30 +0100

    Initial commit - New Rails 3.2.13 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFileUploadsWithJquery::Application.config.secret_token = '4058402ccf1030f3c939fa55e0d787e70d66cac23a5dd3cbfcca298eaf5b893d12a9a5329be82db9881b01b9e5a51813342792b661b8fb52edf5c3600a294fb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsFileUploadsWithJquery::Application.config.session_store :cookie_store, key: '_rails-file-uploads-with-jquery_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsFileUploadsWithJquery::Application.config.session_store :active_record_store
